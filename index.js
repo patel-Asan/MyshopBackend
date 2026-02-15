a0eca85f927d9710e7e985a6dac719ce644e860b
patel-Asan <patelasan91@example.com>
2026-02-15 18:07:15 +0530

    updated cors for netlify

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
- 
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
- 
+
 const app = express();
-app.use(cors());
+
+// ✅ CORS (Allow Netlify frontend)
+app.use(cors({
+  origin: [
+    "http://localhost:5173", // Vite local
+    "https://myshopfront.netlify.app" 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
- 
+
 // Connect DB
 connectDB();
- 
+
 // Routes
 app.use("/", userRoutes);
- 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
- 
- 
- 
+
+// ✅ Use Render Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on port ${PORT}`)
+);
